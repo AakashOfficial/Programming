39dee7d1f2133329f9c325d31b6758e2378610f6
Aakash Tyagi <aakashtyagi1996@outlook.com>
2018-07-24 19:48:00 +0530

    Updated Arithmatic Operation In Python !

--- Python Programming Basics/arithmatic.py
+++ Python Programming Basics/arithmatic.py
@@ -6,3 +6,20 @@ print(a - b)
 print(a * b)
 print(a / b)
 print(a % b)
+
+# Shortcuts For Expressions
+
+a = a + b
+a += b
+
+a = a - b
+a -= b
+
+a = a * b
+a *= b
+
+a = a / b
+a /= b
+
+a = a % b
+a %= b
