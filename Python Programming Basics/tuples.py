c5f4683c76eb880e12f57f95bd74c806ac2797b6
Aakash Tyagi <aakashtyagi1996@outlook.com>
2018-07-25 01:57:14 +0530

    Tuples In Python !

---
+++ Python Programming Basics/tuples.py
@@ -0,0 +1,8 @@
+l = [100,200,300]
+t = {100,200,300}
+
+l.append(400)
+#t.append(400) # Tuples can not be Edited. So, We can not append Tuples. They Are Declared With Paranthesis{}
+
+print(l)
+print(t)
